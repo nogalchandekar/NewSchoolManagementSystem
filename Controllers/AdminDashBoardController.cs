d8e3b32ab35ff674d6971d86c1f191e3bdb5956a
Nogal Chandekar <nogalchandekar88@gmail.com>
2025-10-04 22:44:01 +0530

    Add project files.

---
+++ Controllers/AdminDashBoardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewSchoolManagementSystem.Controllers
+{
+    public class AdminDashBoardController : Controller
+    {
+        // GET: AdminDashBoard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/TeachersLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewSchoolManagementSystem.Controllers
+{
+    public class TeachersLoginController : Controller
+    {
+        // GET: TeachersLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
